ef04910f3348b4aa300e9f874b1b0802c2ecb84e
Gozner <https://github.com/GoznerJ/repo.git>
2017-05-16 18:15:24 +0300

    Files commited

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,20 @@
-import java.io.IOException;
-
-import ui.LaboratoriesUI;
-
-
-/**
- * Created by Gozner on 4/1/2017.
- */
-public class App {
-
-    public static void main(String[] args) {
-        LaboratoriesUI view  = new LaboratoriesUI();
-
-        try {
-            view.run();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//import java.io.IOException;
+//
+//import ui.LaboratoriesUI;
+//
+//
+///**
+// * Created by Gozner on 4/1/2017.
+// */
+//public class App {
+//
+//    public static void main(String[] args) {
+//        LaboratoriesUI view  = new LaboratoriesUI();
+//
+//        try {
+//            view.run();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
